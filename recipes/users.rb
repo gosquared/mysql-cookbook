0ae4c46b52223b3b4a3e1f02c6248f51930cfbf4
Gerhard Lazu <gerhard@lazu.co.uk>
2011-10-06 01:35:42 +0100

    Create db when granting user privileges if missing

--- recipes/users.rb
+++ recipes/users.rb
@@ -5,6 +5,10 @@ node[:mysql][:users].each do |user, properties|
   hosts.each do |host|
     properties[:databases].each do |db|
       mysql_database db do
+        action :create_db
+      end
+
+      mysql_database db do
         new_username user
         new_password properties[:password]
         new_username_host host
