895a9aa8324a14f5d95246a7e764b403164f4ebf
Gerhard Lazu <gerhard@lazu.co.uk>
2012-10-10 08:44:46 +0100

    If WAN is down, DNS resolution won't work

--- recipes/migrate.rb
+++ recipes/migrate.rb
@@ -1,4 +1,4 @@
-wan_up = `ping -c 1 -W 1 google.com`.index(/1 (?:packets )?received/)
+wan_up = `ping -c 1 -W 1 8.8.8.8`.index(/1 (?:packets )?received/)
 
 if wan_up
   node[:mysql][:migrate].each do |host|
